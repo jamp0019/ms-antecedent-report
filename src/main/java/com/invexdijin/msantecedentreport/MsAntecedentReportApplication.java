d77b1e6606a37b0270e955c11b36e77910a9661f
jmartinez79 <john1992alex@gmail.com>
2023-11-22 16:15:15 -0500

    Initial commit

---
+++ src/main/java/com/invexdijin/msantecedentreport/MsAntecedentReportApplication.java
@@ -0,0 +1,13 @@
+package com.invexdijin.msantecedentreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAntecedentReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAntecedentReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invexdijin/msantecedentreport/MsAntecedentReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invexdijin.msantecedentreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAntecedentReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
